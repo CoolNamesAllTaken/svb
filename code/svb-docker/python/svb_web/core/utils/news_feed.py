43506456f28b18a12232ea2da7cdbcbf2cf1f268
Morgan Tenney <morgan.tenney@swiftsolar.com>
2023-10-11 20:49:44 -0700

    publish/unpublish articles

--- code/svb-docker/python/svb_web/core/utils/news_feed.py
+++ code/svb-docker/python/svb_web/core/utils/news_feed.py
@@ -1,24 +1,16 @@
 import datetime
-from core.models import NewsArticle, NewsAuthor
+from core.models import NewsArticle
 
 
 def get_all_newsfeed():
     # return a list of articles in order of publish time
-    return NewsArticle.objects.all().order_by('date_published')
+    return NewsArticle.objects.all().order_by('-date_published')
 
 
 def get_live_newsfeed():
     # return a list of articles with publish time in the past in order of publish time
     all_news_feed = get_all_newsfeed()
     return all_news_feed.filter(date_published__lte=datetime.datetime.now())
-
-
-def create_article(headline: str, author_name: str):
-    # create a new article
-    author = NewsAuthor.objects.get(name=author_name)
-    article = NewsArticle(headline=headline, author=author)
-    article.save()
-    return article.headline
 
 
 def publish_article(headline: str, publish_at: datetime = None):
