cdc0d99d726dc613c6bfa7e73f3c9ec48baafbf2
cahogan <cahogan16@gmail.com>
2023-10-18 22:03:51 -0700

    Add pages for account and account holder overview

--- code/svb-docker/python/svb_web/core/urls.py
+++ code/svb-docker/python/svb_web/core/urls.py
@@ -4,8 +4,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.public.common.index, name="index"),
-    path('account/', views.public.customer.account, name="account"),
+    path('', views.public.common.index, name="index"),
+    path('account_holder/<int:account_holder_number>', views.public.customer.overview, name="account_holder"),
+    path('account_holder', views.public.customer.overview, name="account_holder_generic"),
+    path('account/<int:account_number>', views.public.customer.account, name="account"),
+    path('account', views.public.customer.account, name="account_generic"),
     path('news_editor/', views.internal.author.news_editor),
     path('article/<str:article_headline>/', views.public.visitor.article, name='article'),
 ]
