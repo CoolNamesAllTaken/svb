9cfadea625ddaf2c7249ccbddb1eba350aea51b0
Paul <pbwalter2@gmail.com>
2023-10-28 17:06:14 -0400

    stub: add keys for HTML context dict

--- code/svb-docker/python/svb_web/core/views/public/common.py
+++ code/svb-docker/python/svb_web/core/views/public/common.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {}
+    context = {
+        "num_ids_created": 0,
+        "num_windows": 0,
+        "wait_time": 0,
+    }
     return render(request, "public/index.html", context)
