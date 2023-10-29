227adb320281411dbd7b3e8c1607e25f5e728534
cahogan <cahogan16@gmail.com>
2023-10-28 18:11:45 -0700

    Style and structure main page

--- code/svb-docker/python/svb_web/core/views/public/common.py
+++ code/svb-docker/python/svb_web/core/views/public/common.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+import core.models
 
 # Create your views here.
 def index(request):
-    context = {}
+    articles = core.models.NewsArticle.objects.all()
+    context = {
+                "articles": articles
+               }
     return render(request, "public/index.html", context)
