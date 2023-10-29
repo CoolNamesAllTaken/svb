2c384c9a03cf5d4c98c8104d1b700c4d0373787f
cahogan <cahogan16@gmail.com>
2023-10-28 21:15:11 -0700

    Test calculate account balance at given timestamp

--- code/svb-docker/python/svb_web/core/models.py
+++ code/svb-docker/python/svb_web/core/models.py
@@ -79,8 +79,8 @@ class Account(models.Model):
         interest_rate = most_recent_event_at_timestamp.interest_rate
         anchor_event_timestamp = most_recent_event_at_timestamp.timestamp
         anchor_event_balance = most_recent_event_at_timestamp.balance
-        intervals_since_anchor_event = (timestamp - anchor_event_timestamp) / COMPOUNDING_INTERVAL_SECONDS
-        balance_at_timestamp = anchor_event_balance * math.exp(interest_rate * intervals_since_anchor_event)
+        intervals_since_anchor_event = (timestamp - anchor_event_timestamp).total_seconds() / COMPOUNDING_INTERVAL_SECONDS
+        balance_at_timestamp = float(anchor_event_balance) * math.exp(interest_rate * intervals_since_anchor_event)
         return balance_at_timestamp
 
     
