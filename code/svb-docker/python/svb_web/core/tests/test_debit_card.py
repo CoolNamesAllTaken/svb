e579daedccc36cb33394521b322dd3c1f824b5c9
John McNelly <jkailimcnelly@gmail.com>
2023-10-30 02:12:03 -0700

    Integrate ID card generation and preview

--- code/svb-docker/python/svb_web/core/tests/test_debit_card.py
+++ code/svb-docker/python/svb_web/core/tests/test_debit_card.py
@@ -6,3 +6,6 @@ import core.utils.debit_card as debit_card_utils
 def test_parse_customer_id_from_url():
     assert debit_card_utils.parse_customer_id_from_url("svb.pantsforbirds.com/c/potatoes") == "potatoes"
     assert debit_card_utils.parse_customer_id_from_url("hello/potatoes") == None
+    assert debit_card_utils.parse_customer_id_from_url("https://svb.pantsforbirds.com/c/doot") == "doot"
+    assert debit_card_utils.parse_customer_id_from_url("http://svb.pantsforbirds.com/c/poton") == "poton"
+    assert debit_card_utils.parse_customer_id_from_url("http://svb.pantsforbirds.com/c/potdon/after") == "potdon"
