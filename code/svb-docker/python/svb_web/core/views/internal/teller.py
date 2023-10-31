1d42bd125ff4d97199cb68514746f2a7ef96ca8d
cahogan <cahogan16@gmail.com>
2023-10-30 23:19:43 -0700

    Add unstyled teller page and withdrawal functionality

--- code/svb-docker/python/svb_web/core/views/internal/teller.py
+++ code/svb-docker/python/svb_web/core/views/internal/teller.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-from core.models import Customer
+import core.models
 from django.contrib.auth.decorators import login_required
-from core.forms import CustomerLookupForm
-
+import core.forms
 
 @login_required
 def lookup_accounts(request, customer_id=None):
@@ -16,14 +15,14 @@ def lookup_accounts(request, customer_id=None):
     message = "Lookup customer by ID."
     if request.method == 'POST':
         # Create a form instance and populate it with data from the request (binding):
-        form = CustomerLookupForm(request.POST)
+        form = core.forms.CustomerLookupForm(request.POST)
 
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
             customer_id = form.cleaned_data['customer_id']
             try:
-                customer = get_object_or_404(Customer, pk=customer_id)
+                customer = get_object_or_404(core.models.Customer, pk=customer_id)
                 # redirect to a new URL:
                 return HttpResponseRedirect('/internal/accounts/edit/' + customer_id + '/')
             except:
@@ -31,7 +30,7 @@ def lookup_accounts(request, customer_id=None):
         else:
             message = "Form contents not valid. Try again!"
     else:
-        form = CustomerLookupForm()
+        form = core.forms.CustomerLookupForm()
     
     context = {
         'form': form,
@@ -41,7 +40,45 @@ def lookup_accounts(request, customer_id=None):
     return render(request, "internal/lookup_accounts.html", context)
 
 
+def make_withdrawal(request):
+    form_account_number = request.POST['account_number']
+    withdrawal_amount = int(request.POST['withdrawal_amount'])
+    account_to_withdraw_from = core.models.Account.objects.get(account_number=form_account_number)
+    bank_reserve_account = core.models.Account.objects.get(account_name="RESERVES")
+    bank_disbursed_account = core.models.Account.objects.get(account_name="DISBURSED")
+    success = core.models.Account.transfer_funds(account_to_withdraw_from, bank_disbursed_account, withdrawal_amount)
+    if success:
+        # This could possibly create a race condition
+        current_reserves = bank_reserve_account.get_balance()
+        core.models.AnchorEvent(account=bank_reserve_account, 
+                                balance= current_reserves - withdrawal_amount,
+                                category="WITHDRAWAL").save()
+        return ["Account withdrawal succeeded."]
+    else:
+        return ["Account withdrawal failed."]
+
+
+def teller_actions(request):
+    action = request.POST['action']
+    if action == 'make_withdrawal':
+        return make_withdrawal(request)
+
+
 @login_required
 def edit_accounts(request, customer_id=None):
-    context = {}
+    # can assume customer_id is valid
+    messages = []
+    if request.method == 'POST':
+        if 'action' in request.POST:
+            messages = teller_actions(request)
+    customer = core.models.Customer.objects.get(pk=customer_id)
+    context = {
+        "customer": customer,
+        "messages": messages,
+        "account_withdrawal_entries": [{
+                                        "form": core.forms.MakeWithdrawalForm(initial={'account_number': account.account_number}),
+                                        "account": account,
+                                        "current_balance": account.get_balance()
+         } for account in customer.account_set.all()]
+    }
     return render(request, "internal/teller.html", context)
